d5398710779aea3489c948ccd92bf600e93c556c
chanyu1 <haneul1ij@gmail.com>
2021-06-07 23:18:02 +0900

    Add cookieSession

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 require("./models/User");
 require("./services/passport");
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+// 초기화
+app.use(passport.initialize());
+// 세션(session)이란 웹 사이트의 여러 페이지에 걸쳐 사용되는 사용자 정보를 저장하는 방법을 의미합니다.
+// 사용자가 브라우저를 닫아 서버와의 연결을 끝내는 시점까지를 세션이라고 합니다.
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
