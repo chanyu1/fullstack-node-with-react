e3a68cbf8dd909552e32edd4d3a177730c21dd87
chanyu1 <haneul1ij@gmail.com>
2021-06-17 00:17:35 +0900

    Add actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
-const Header = () => <h2>Header</h2>;
+import Header from "./components/Header";
+
+const Landing = () => <h2>Landing</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Landing}></Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
 
 import App from "./App";
 import reducers from "./_reducers";
