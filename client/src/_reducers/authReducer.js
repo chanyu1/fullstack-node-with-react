587a3114b50c2035568358c7fa652756a6327cbf
chanyu1 <haneul1ij@gmail.com>
2021-06-17 21:40:05 +0900

    Add dispatch

--- client/src/_reducers/authReducer.js
+++ client/src/_reducers/authReducer.js
@@ -1,5 +1,12 @@
-export default function (state = {}, action) {
+import { FETCH_USER } from "../_actions/types";
+
+// eslint-disable-next-line
+export default function (state = null, action) {
+  console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+      // '' || false => false
+      return action.payload;
     default:
       return state;
   }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import ReduxThunk from "redux-thunk";
+import reduxThunk from "redux-thunk";
 
 import App from "./App";
 import reducers from "./_reducers";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
