dc6cc4a4e3c6334affd0dd2d1d32aa11d382c557
chanyu1 <haneul1ij@gmail.com>
2021-06-21 20:17:17 +0900

    Fix billingRoutes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const keys = require("./config/keys");
+const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const bodyParser = require("body-parser");
+const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
@@ -34,6 +34,18 @@ mongoose
   })
   .then(() => console.log("MongoDB connected!"))
   .catch((err) => console.log(err));
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Your app listening on port ${PORT}!`));
