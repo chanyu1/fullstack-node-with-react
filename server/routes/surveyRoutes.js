53dd1936f9c34f0a44e427e7c2f2b47e6d214d51
chanyu1 <haneul1ij@gmail.com>
2021-07-23 23:34:31 +0900

    Fix surveyRoutes

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,14 +10,22 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false,
+    });
+
+    res.send(surveys);
+  });
+
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting!");
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
     const p = new Path("/api/surveys/:surveyId/:choice");
 
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
@@ -31,9 +39,23 @@ module.exports = (app) => {
       })
       .compact() // Remove the elements that are 'undefined'
       .uniqBy("email", "surveyId") // Remove records with duplicate email and surveyId
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false },
+            },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date(),
+          }
+        ).exec();
+      })
       .value();
 
-    console.log(events);
     res.send({});
   });
 
@@ -46,6 +68,7 @@ module.exports = (app) => {
       body,
       recipients: recipients.split(",").map((email) => ({
         email: email.trim(),
+        responded: false,
       })),
       _user: req.user.id,
       dateSent: Date.now(),
